b4611d9f85b9f37174860568ca1db8de0f2c5420
Antara Arifa <antara5643@gmail.com>
2023-10-18 10:14:52 +0600

    3rd commit

--- SmartCds.cpp
+++ SmartCds.cpp
@@ -5,15 +5,13 @@ using namespace std;
 
 void login();
 void registration();
+void display();
 
 int main()
 {
    int option;
 
-   cout << "################################################################\n" <<endl;
-   cout << "########################## SMARTCDS ############################\n" <<endl;
-   cout << "################################################################\n" <<endl;
-   cout << "================================================================\n" <<endl;
+   display();
    cout << "---------------------- Welcome to SMARTCDS ---------------------\n" <<endl;
    cout << "================================================================\n" <<endl;
    cout << "1. REGISTER\n" <<endl;
@@ -48,6 +46,8 @@ int main()
    }
 }
 
+
+
    void login()
    {
        int  option;
@@ -55,10 +55,7 @@ int main()
        void loginAsSalesperson();
        void loginAsCustomer();
 
-       cout << "###########################################################\n" << endl;
-       cout << "######################### SMARTCDS ########################\n" << endl;
-       cout << "###########################################################\n" << endl;
-       cout << "===========================================================\n" << endl;
+       display();
        cout << "-------------------------- LOGIN --------------------------\n" << endl;
        cout << "===========================================================\n" << endl;
        cout << "1. As an ADMIN\n" << endl;
@@ -73,12 +70,15 @@ int main()
 
        switch (option) {
         case 1:
+            system("cls");
             loginAsAdmin();
             break;
         case 2:
+            system("cls");
             loginAsSalesperson();
             break;
         case 3:
+            system("cls");
             loginAsCustomer();
             break;
         case 4:
@@ -101,26 +101,25 @@ int main()
        void registerAsAdmin();
        void registerAsCustomer();
 
-       cout << "###########################################################\n" << endl;
-       cout << "######################### SMARTCDS ########################\n" << endl;
-       cout << "###########################################################\n" << endl;
-       cout << "===========================================================\n" << endl;
-       cout << "------------------------- REGISTER ------------------------\n" << endl;
-       cout << "===========================================================\n" << endl;
+       display();
+       cout << "--------------------------- REGISTER --------------------------\n" << endl;
+       cout << "===============================================================\n" << endl;
        cout << "1. As an ADMIN\n" << endl;
        cout << "2. As a CUSTOMER\n" << endl;
        cout << "3. Return to Welcome Page\n" << endl;
        cout << "4. EXIT\n" << endl;
-       cout << "***********************************************************" << endl;
+       cout << "***************************************************************" << endl;
 
        cin >> option;
        cout << endl;
 
        switch (option) {
         case 1:
+            system("cls");
             registerAsAdmin();
             break;
         case 2:
+            system("cls");
             registerAsCustomer();
             break;
         case 3:
@@ -140,6 +139,10 @@ int main()
 
    void registerAsAdmin()
    {
+    display();
+    cout << "--------------------------- REGISTER --------------------------\n" << endl;
+    cout << "===============================================================\n" << endl;
+
     string username, password;
 
     cout << "Enter  username: ";
@@ -164,6 +167,10 @@ int main()
    }
    void registerAsCustomer()
    {
+    display();
+    cout << "--------------------------- REGISTER --------------------------\n" << endl;
+    cout << "===============================================================\n" << endl;
+
     string username, password;
 
     cout << "Enter a username: ";
@@ -187,19 +194,21 @@ int main()
    }
    void loginAsAdmin()
    {
+       display();
+       cout << "----------------------------- LOGIN -----------------------------\n" << endl;
+       cout << "=================================================================\n" << endl;
+
        int count;
 
        string userID, password, id, pass;
 
-       system("cls");
-
-       cout<<"\t\t\t Please enter your username and password:"<<endl;
+       cout<<"\t\t\t Please enter your username and password "<<endl;
 
        cout<<"USERNAME: ";
 
        cin>>userID;
 
-       cout<<"PASSWORD";
+       cout<<"PASSWORD: ";
 
        cin>>password;
 
@@ -218,11 +227,11 @@ int main()
 
        if(count==1)
        {
-           cout<<userID<<"\n Your login is successful\n Thanks for logging in \n";
-           main();
+           cout<<userID<<" your login is successful\n Thanks for logging in \n";
+
        }else
        {
-           cout<<"\n LOGIN ERROR \n Please check username and password \n";
+           cout<<" LOGIN ERROR \n Please check username and password \n";
        }
 
 
@@ -233,19 +242,21 @@ int main()
    }
    void loginAsCustomer()
    {
-        int count;
+       display();
+       cout << "----------------------------- LOGIN -----------------------------\n" << endl;
+       cout << "=================================================================\n" << endl;
+
+       int count;
 
        string userID, password, id, pass;
 
-       system("cls");
-
-       cout<<"\t\t\t Please enter your username and password:"<<endl;
+       cout<<"\t\t\t Please enter your username and password"<<endl;
 
        cout<<"USERNAME: ";
 
        cin>>userID;
 
-       cout<<"PASSWORD";
+       cout<<"PASSWORD: ";
 
        cin>>password;
 
@@ -264,12 +275,21 @@ int main()
 
        if(count==1)
        {
-           cout<<userID<<"\n Your login is successful\n Thanks for logging in \n";
-           main();
+           cout<<userID<<" your login is successful\n Thanks for logging in \n";
+
        }else
        {
-           cout<<"\n LOGIN ERROR \n Please check username and password \n";
+           cout<<" LOGIN ERROR \n Please check username and password \n";
        }
+
+   }
+
+   void display()
+   {
+       cout << "################################################################\n" <<endl;
+       cout << "########################## SMARTCDS ############################\n" <<endl;
+       cout << "################################################################\n" <<endl;
+       cout << "================================================================\n" <<endl;
    }
 
 
